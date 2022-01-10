a27830cc675cbc6ada5b873fb28122a0e9ba1b33
Gregory F Martin <greg@gregfmartin.blog>
2022-01-09 23:52:59 -0500

    I've been able to scale, rotate, and pan the camera in all different directions and degrees.

--- Sample SpriteKit Game Shared/Source/TileMapScene.swift
+++ Sample SpriteKit Game Shared/Source/TileMapScene.swift
@@ -35,6 +35,8 @@ class TileMapScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         if let cam = self.localCamera {
             print("Current camera position is \(cam.position.x), \(cam.position.y)")
+            print("Current camera size is \(cam.xScale), \(cam.yScale)")
+            print("Current camera rotation is \(cam.zRotation)")
         }
     }
 }
@@ -47,27 +49,65 @@ extension TileMapScene {
                 switch Int(kc) {
                 case NSUpArrowFunctionKey:
                     if let cam = self.localCamera {
-                        cam.run(SKAction.moveBy(x: 0.0, y: 9.0, duration: 0.5))
+                        cam.run(SKAction.moveBy(x: 0.0, y: 25.0, duration: 0.5))
                     }
                     break
                 case NSDownArrowFunctionKey:
                     if let cam = self.localCamera {
-                        cam.run(SKAction.moveBy(x: 0.0, y: -9.0, duration: 0.5))
+                        cam.run(SKAction.moveBy(x: 0.0, y: -25.0, duration: 0.5))
                     }
                     break
                 case NSLeftArrowFunctionKey:
                     if let cam = self.localCamera {
-                        cam.run(SKAction.moveBy(x: -9.0, y: 0.0, duration: 0.5))
+                        cam.run(SKAction.moveBy(x: -25.0, y: 0.0, duration: 0.5))
                     }
                     break
                 case NSRightArrowFunctionKey:
                     if let cam = self.localCamera {
-                        cam.run(SKAction.moveBy(x: 9.0, y: 0.0, duration: 0.5))
+                        cam.run(SKAction.moveBy(x: 25.0, y: 0.0, duration: 0.5))
                     }
                     break
                 default:
                     break
                 }
+            }
+        }
+        
+        if let k = event.charactersIgnoringModifiers {
+            switch k {
+            case "a", "A":
+                if let cam = self.localCamera {
+                    cam.run(SKAction.scale(to: 0.5, duration: 0.2))
+                }
+                break
+            case "s", "S":
+                if let cam = self.localCamera {
+                    cam.run(SKAction.scale(to: 1.0, duration: 0.2))
+                }
+                break
+            case "z", "Z":
+                if let cam = self.localCamera {
+//                    cam.run(SKAction.rotate(toAngle: 90.0 * .pi / 180, duration: 0.25))
+                    cam.run(SKAction.rotate(byAngle: 90.0 * .pi / 180, duration: 0.25))
+                }
+                break
+            case "x", "X":
+                if let cam = self.localCamera {
+                    cam.run(SKAction.rotate(byAngle: -(90.0 * .pi / 180), duration: 0.25))
+                }
+                break
+            case "c", "C":
+                if let cam = self.localCamera {
+                    cam.run(SKAction.rotate(byAngle: 360.0 * .pi / 180, duration: 0.25))
+                }
+                break
+            case "v", "V":
+                if let cam = self.localCamera {
+                    cam.run(SKAction.rotate(byAngle: -(360.0 * .pi / 180), duration: 0.25))
+                }
+                break
+            default:
+                break
             }
         }
     }
